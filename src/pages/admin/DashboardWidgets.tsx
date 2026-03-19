ca1ef39025334188f723993edc6a147859b51f43
dick@socialhp.com <dick@socialhp.com>
2026-03-19 17:26:27 +0800

    Restructured admin pages

--- src/pages/admin/DashboardWidgets.tsx
+++
@@ -1,3 +0,0 @@
-export default function DashboardWidgets() {
-  return <h1>Dashboard Widgets</h1>;
-}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,16 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import Feedback from "../pages/feedback/Feedback";
-import Dashboard from "../pages/admin/Dashboard";
+import Admin from "../pages/admin/Admin";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/feedback" element={<Feedback />} />
-      <Route path="/admin">
-        <Route path="dashboard" element={<Dashboard />} />
-      </Route>
+      <Route path="/admin" element={<Admin />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );

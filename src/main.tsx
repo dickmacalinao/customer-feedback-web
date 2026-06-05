1e2e3741e1d6d139f1bea8bd68b1fcf280660142
dick@socialhp.com <dick@socialhp.com>
2026-06-06 00:32:24 +0800

    Removed reference from react-cookie

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { CookiesProvider } from "react-cookie";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -8,9 +7,7 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <CookiesProvider>
-        <App />
-      </CookiesProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
